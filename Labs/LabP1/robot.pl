736c33ea20e9e50614040d08ab242e3b271858d3
DEJohansson <33880373+DEJohansson@users.noreply.github.com>
2019-05-02 14:30:20 +0200

    LabP1: Ready for presentation.

--- Labs/LabP1/robot.pl
+++ Labs/LabP1/robot.pl
@@ -1,15 +1,48 @@
-move(state(KeyB, KeyS, Package, Robot), NewState, Action) :-
-    Robot = r1,
-    KeyS = r1,
-    NewState = state(KeyB, r2, Package, r2),
-    Action = [keyB, nothing, r2].
+% David Johansson
+
+moveItem(state(KeyB, KeyS, Room, Room), NewRoom, NewState, package) :-
+    NewState = state(KeyB, KeyS, NewRoom, NewRoom).
+
+moveItem(state(KeyB, Room, Package, Room), NewRoom, NewState, keyS) :-
+    NewState = state(KeyB, NewRoom, Package, NewRoom).
+
+moveItem(state(Room, KeyS, Package, Room), NewRoom, NewState, keyB) :-
+    NewState = state(NewRoom, KeyS, Package, NewRoom).
+
+moveItem(state(KeyB, KeyS, Package, _), NewRoom, NewState, nothing) :-
+    NewState = state(KeyB, KeyS, Package, NewRoom).
+
+move(state(KeyB, r1, Package, r1), NewState, Action) :-
+    moveItem(state(KeyB, r2, Package, r1), r2, State, Item),
+    NewState = State,
+    Action = move(keyS, Item, r2).
+
+move(state(r1, KeyS, Package, r1), NewState, Action) :-
+    moveItem(state(r3, KeyS, Package, r1), r3, State, Item),
+    NewState = State,
+    Action = move(keyB, Item, r3).
+
+move(state(KeyB, r2, Package, r2), NewState, Action) :-
+    moveItem(state(KeyB, r1, Package, r2), r1, State, Item),
+    NewState = State,
+    Action = move(keyS, Item, r1).
+
+move(state(r3, KeyS, Package, r3), NewState, Action) :-
+    moveItem(state(r1, KeyS, Package, r3), r1, State, Item),
+    NewState = State,
+    Action = move(keyB, Item, r1).
 
 %state(KeyB, KeyS, Package, Robot)
 solveR(state(_, _, r2, _), _, []). % Package in room 2.
 solveR(State, N, Action) :-
     N>0,
-    move(State, NewState, [Key, Item, Room]),
-    solveR(NewState, N-1, Actions),
-    Action = [move(Key, Item, Room) | Actions].
+    move(State, NewState, NewAction),
+    solveR(NewState, N-1, Trace),
+    Action = [NewAction | Trace].
 
+% Test
 start(Action) :- solveR(state(r2, r1, r3, r1), 5, Action).
+
+% Extra tests 
+start2(Action) :- solveR(state(r1, r3, r1, r1), 3, Action).
+start3(Action) :- solveR(state(r1, r3, r3, r1), 5, Action).
