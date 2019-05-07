d4d56061388fa8ffd3e45dab7fc45c3633f49529
DEJohansson <33880373+DEJohansson@users.noreply.github.com>
2019-05-07 18:05:33 +0200

    LabP2: Added takeKtoOne.

--- Labs/LabP2/LabP2.pl
+++ Labs/LabP2/LabP2.pl
@@ -8,3 +8,17 @@ iSort([], []).
 iSort([X| Tail], Sorted) :- 
     iSort(Tail, TailS),
     ins(X, TailS, Sorted).
+
+take(1, [H|_], [H]).
+take(K, [H|T], [H|T2]) :- K > 1, K2 is K-1, take(K2, T, T2).
+
+sum([X], X).
+sum([X, Y | T], Tot) :- Sum is X+Y, sum([Sum|T], Tot).
+
+takeKtoOne(S, 1, [X|_], [([X], X, S, S)]).
+takeKtoOne(S, K, L, [(Seq, Sum, S, E) | Seqs]) :- 
+    K > 1, take(K, L, Seq), 
+    sum(Seq, Sum), K2 is K-1, 
+    E is S+K2, takeKtoOne(S, K2, L, Seqs).
+
+ 
