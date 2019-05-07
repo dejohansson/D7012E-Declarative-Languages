2ea60dbd7512c8f71ab7bf5a22d3570849499b05
DEJohansson <33880373+DEJohansson@users.noreply.github.com>
2019-05-07 16:52:54 +0200

    LabP2: Added iSort.

--- Labs/LabP2/LabP2.pl
+++ Labs/LabP2/LabP2.pl
@@ -1,2 +1,10 @@
 % David Johansson
 
+ins(X, [], [X]).
+ins(X, [Y| Tail], [X, Y | Tail]) :- X =< Y.
+ins(X, [Y| Tail], [Y|Tail1]) :- X > Y, ins(X, Tail, Tail1).
+
+iSort([], []).
+iSort([X| Tail], Sorted) :- 
+    iSort(Tail, TailS),
+    ins(X, TailS, Sorted).
